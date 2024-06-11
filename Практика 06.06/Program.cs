5ea370eaba9ff3e4e8bbcbc7cd86a0756340d4af
F1eshka <144143888+F1eshka@users.noreply.github.com>
2024-06-11 15:14:00 +0300

    Add files via upload

---
+++ Практика 06.06/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Практика_06._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Краткое резюме Беноевой Малики, П26 \n23.07.2006\n");
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.WriteLine("Smm специалист со стажем работы 6 месяцев");
+            Console.WriteLine("Прошла обучение у @angelina.pyliava и имею сертификат об успешном окончании курсов\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Навыки:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Настройка и запуск таргетированной рекламы");
+            Console.WriteLine("Создание визуала");
+            Console.WriteLine("Создание сторис");
+            Console.WriteLine("Постинг контента");
+            Console.WriteLine("Написание текста для постов");
+            Console.WriteLine("Создание личного стиля");
+            Console.WriteLine("Переписки в директ");
+            Console.WriteLine("Создание контент плана");
+            Console.WriteLine("Дизайн контента");
+            Console.WriteLine("Анализ целевой аудитории");
+            Console.WriteLine("Анализ конкурентов\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Языки:\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Украинский (носитель)\nРусский (носитель)\nАнглийский (уровень В1)\n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Контакты:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Номер телефона: +380734054475");
+            Console.WriteLine("Почта: bm.work07@gmail.com\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t Спасибо за внимание!!! \t\t");
+
+
+
+        }
+    }
+}
